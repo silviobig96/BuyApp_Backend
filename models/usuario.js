f8941c107c0d8318a0597aa8a394d96754a2feb0
unknown <silviobig96@hotmail.com>
2019-10-17 01:06:56 -0400

    commit

--- models/usuario.js
+++ models/usuario.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const usuario_schema = new Schema({
 
-    email: {type: String, required: [true, 'El nombre es obligatorio'], },
+    email: {type: String, required: [true, 'El email es obligatorio'] },
     contraseña: {type: String, required: [true, 'Contraseña requerida']},
     fecha_creacion:{type: Date, default: Date.now},
     estado: {type:Boolean, default:true}
